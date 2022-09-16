eb0d24bcdb6d52cd48e027343709ed284ffa482d
Jorge Colmenarez <jlct.master@gmail.com>
2022-09-16 11:30:37 -0400

    Fixed bug when calculate Tolerance Percent

--- net.frontuari.recordweight/src/net/frontuari/recordweight/model/MFTURecordWeight.java
+++ net.frontuari.recordweight/src/net/frontuari/recordweight/model/MFTURecordWeight.java
@@ -276,7 +276,9 @@ public class MFTURecordWeight extends X_FTU_RecordWeight implements DocAction, D
 		if(getOperationType().equalsIgnoreCase(OPERATIONTYPE_DeliveryFinishedProduct) && !isApproved())
 		{
 			BigDecimal oNetWeight = getFTU_LoadOrder().getWeight();
-			BigDecimal toleranceAmt = (new BigDecimal(tolerancePercentage).multiply(oNetWeight)).divide(Env.ONEHUNDRED, 0,RoundingMode.HALF_UP);
+			
+			BigDecimal toleranceAmt = (new BigDecimal(tolerancePercentage).divide(Env.ONEHUNDRED, 2,RoundingMode.HALF_UP));
+			toleranceAmt = oNetWeight.multiply(toleranceAmt);
 			BigDecimal difference = getNetWeight().subtract(oNetWeight);
 			if(difference.compareTo(toleranceAmt.negate()) == -1 
 					|| difference.compareTo(toleranceAmt) == 1)
@@ -285,7 +287,7 @@ public class MFTURecordWeight extends X_FTU_RecordWeight implements DocAction, D
 				//	Support for write QtyDifference
 				DB.executeUpdate("UPDATE FTU_RecordWeight SET DifferenceQty="+difference+" WHERE FTU_RecordWeight_ID = ?", get_ID(), get_TrxName());
 				//	End Jorge Colmenarez
-				m_processMsg = "El peso neto ["+getNetWeight()+"] no puede exceder la capacidad de carga ["+oNetWeight+"], diferencia= "+difference+", tolerancia = "+toleranceAmt+" se requiere una autorizacion.";
+				m_processMsg = "El peso neto ["+getNetWeight()+"] no puede exceder la capacidad de carga ["+oNetWeight+"], diferencia= "+difference+", tolerancia = ["+tolerancePercentage+"% = "+toleranceAmt+"] se requiere una autorizacion.";
 				return DocAction.STATUS_WaitingConfirmation;
 			}
 		}
