03e39d10cdc3e4a73fe6323940dd52dcd4ddb6ef
DavidCastillo20 <davidcastillo118@gmail.com>
2023-07-13 18:28:49 -0400

    convertion

--- net.frontuari.recordweight/src/net/frontuari/recordweight/process/FTUGenerateInvoiceFromFreightCost.java
+++ net.frontuari.recordweight/src/net/frontuari/recordweight/process/FTUGenerateInvoiceFromFreightCost.java
@@ -1,13 +1,16 @@
 package net.frontuari.recordweight.process;
 
+import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.sql.Timestamp;
 
 import org.adempiere.base.annotation.Process;
 import org.adempiere.exceptions.AdempiereException;
 import org.compiere.model.MBPartner;
+import org.compiere.model.MConversionRate;
 import org.compiere.model.MInvoice;
 import org.compiere.model.MInvoiceLine;
+import org.compiere.model.MPriceList;
 import org.compiere.process.ProcessInfoParameter;
 import org.compiere.util.Env;
 
@@ -21,6 +24,7 @@ public class FTUGenerateInvoiceFromFreightCost extends FTUProcess{
 	
 	private int p_C_DocType_ID = 0;
 	private int p_C_DocTypeCreditNote_ID = 0;
+	private int p_M_PriceList_ID = 0;
 	@Override
 	protected void prepare() {
 		
@@ -32,7 +36,8 @@ public class FTUGenerateInvoiceFromFreightCost extends FTUProcess{
 				p_C_DocType_ID = para.getParameterAsInt();
 			else if(name.equals("C_DocTypeTarget_ID"))
 				p_C_DocTypeCreditNote_ID = para.getParameterAsInt();
-			//else if (name.equals("C_DocType_ID"))
+			else if(name.equals("M_PriceList_ID"))
+				p_M_PriceList_ID = para.getParameterAsInt();
 			
 		}
 		
@@ -53,6 +58,8 @@ public class FTUGenerateInvoiceFromFreightCost extends FTUProcess{
 		MFTUFreightCostLine[] fcLines = fc.getLines(" AND DiscountWeight <= 0");
 		MFTUFreightCostLine[] cnLines = fc.getLines(" AND DiscountWeight > 0 ");
 		
+		MPriceList pl = new MPriceList(getCtx(), p_M_PriceList_ID, get_TrxName());
+		int c_currency_id = pl.getC_Currency_ID();
 		
 		if (fcLines.length>0) {
 			inv = new MInvoice(getCtx(), 0, get_TrxName());
@@ -60,6 +67,8 @@ public class FTUGenerateInvoiceFromFreightCost extends FTUProcess{
 			inv.setC_DocTypeTarget_ID(p_C_DocType_ID);
 			inv.setBPartner((MBPartner) fc.getM_Shipper().getC_BPartner());
 			inv.setSalesRep_ID(getAD_User_ID());
+			inv.setM_PriceList_ID(p_M_PriceList_ID);
+			//inv.setC_Currency_ID(fc.getC_Currency_ID());
 			inv.setDateInvoiced(now);
 			inv.setDateAcct(now);
 			inv.saveEx();
@@ -67,7 +76,12 @@ public class FTUGenerateInvoiceFromFreightCost extends FTUProcess{
 			for (MFTUFreightCostLine fcline : fcLines) {
 				MInvoiceLine line = new MInvoiceLine(inv);
 				line.setC_Charge_ID(fcline.getC_Charge_ID());
-				line.setPrice(fcline.getCosts());
+				if (c_currency_id != fc.getC_Currency_ID()) {
+					BigDecimal amt = MConversionRate.convert(getCtx(), fcline.getCosts(), fc.getC_Currency_ID(), c_currency_id,now,fc.getC_ConversionType_ID() ,fc.getAD_Client_ID(), fc.getAD_Org_ID());
+					line.setPrice(amt);
+				}else {
+					line.setPrice(fcline.getCosts());
+				}
 				line.setQty(Env.ONE);
 				line.saveEx();
 			}
@@ -79,6 +93,8 @@ public class FTUGenerateInvoiceFromFreightCost extends FTUProcess{
 			cn.setC_DocTypeTarget_ID(p_C_DocTypeCreditNote_ID);
 			cn.setBPartner((MBPartner) fc.getM_Shipper().getC_BPartner());
 			cn.setSalesRep_ID(getAD_User_ID());
+			cn.setM_PriceList_ID(p_M_PriceList_ID);
+			//cn.setC_Currency_ID(fc.getC_Currency_ID());
 			cn.setDateInvoiced(now);
 			cn.setDateAcct(now);
 			cn.set_ValueOfColumn("LVE_invoiceAffected_ID", inv.getC_Invoice_ID());
@@ -87,7 +103,13 @@ public class FTUGenerateInvoiceFromFreightCost extends FTUProcess{
 			for (MFTUFreightCostLine fcline : cnLines) {
 				MInvoiceLine line = new MInvoiceLine(cn);
 				line.setC_Charge_ID(fcline.getC_Charge_ID());
-				line.setPrice(fcline.getPriceActual().multiply(fcline.getDiscountWeight()).setScale(4,RoundingMode.HALF_UP));
+				if (c_currency_id != fc.getC_Currency_ID()) {
+					
+					BigDecimal amt = MConversionRate.convert(getCtx(), fcline.getPriceActual().multiply(fcline.getDiscountWeight()).setScale(4,RoundingMode.HALF_UP), fc.getC_Currency_ID(), c_currency_id,now,fc.getC_ConversionType_ID() ,fc.getAD_Client_ID(), fc.getAD_Org_ID());
+					line.setPrice(amt);
+				}else {
+					line.setPrice(fcline.getPriceActual().multiply(fcline.getDiscountWeight()).setScale(4,RoundingMode.HALF_UP));
+				}
 				line.setQty(Env.ONE);
 				line.saveEx();
 			}
